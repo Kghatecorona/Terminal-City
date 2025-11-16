91ac7ae75e005ee10727a5b7d855b7addb868d02
Shristi Jain <shristijain941@gmail.com>
2025-11-16 13:59:08 +0530

    Added pure virtual function

---
+++ include/building.h
@@ -0,0 +1,20 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+
+// Abstract base class for all building types
+class Building {
+public:
+    // Pure virtual function to get building symbol
+    virtual char getSymbol() const = 0;
+    
+    // Pure virtual function to get production per turn
+    virtual int produceGold() const = 0;
+    
+    // Get building name for logging
+    virtual const char* getName() const = 0;
+    
+    // Virtual destructor
+    virtual ~Building() {}
+};
+
+#endif // BUILDING_H     
