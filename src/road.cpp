9f25cfd2fd5710b30519bf699d0825dc19dda885
wp1710 <itswp.1710@gmail.com>
2025-11-17 23:19:26 +0530

    Added road implementation

---
+++ src/road.cpp
@@ -0,0 +1,13 @@
+#include "road.h"
+
+char Road::getSymbol() const {
+    return '=';
+}
+
+int Road::produceGold() const {
+    return 0;
+}
+
+const char* Road::getName() const {
+    return "Road";
+}
